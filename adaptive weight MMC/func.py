2041ac3a1413bd8ea254cbe53b3a764b4483834b
chenzu <chen0zu@gmail.com>
2019-01-17 22:35:32 +0800

    update

---
+++ adaptive weight MMC/func.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 21:46:30 2019
+
+@author: chenzu
+"""
+
+import numpy as np
+
+def mmc(X,gnd):
+    St = np.cov(X.T, bias = True)
+    N,D = X.shape
+    Sw = np.zeros([D,D])
+    for i in np.unique(gnd):
+        Sw = Sw + sum(gnd==i)/N*np.cov(X[gnd.reshape(-1) == i,:].T, bias = True)
+    Sb = St - Sw
+    
+    eig_value, eig_vec = np.linalg.eig(Sb-Sw)
+    idx = np.argsort(-eig_value)
+    return eig_value[idx], eig_vec[:,idx]
+
+def weightmmc(X,gnd,d,alpha = 1):
+    Iter = 10
+    
+    St = np.cov(X.T,bias=True)
+    N,D = X.shape
+    Sw = np.zeros([D,D])
+    for i in np.unique(gnd):
+        Sw = Sw + sum(gnd==i)/N*np.cov(X[gnd.reshape(-1) == i,:].T, bias = True)
+    Sb = St - Sw
+    
+    obj = np.zeros([Iter, 2])
+    for i in range(Iter):
+        # update projection vector
+        eig_value, eig_vec = np.linalg.eig(alpha*Sb-alpha**2*Sw)
+        idx = np.argsort(-eig_value)
+        eig_value = eig_value[idx[0:d]]
+        eig_vec = eig_vec[:,idx[0:d]]
+        
+        # update alpha
+        alpha = np.trace(eig_vec.T@Sb@eig_vec)/(2*np.trace(eig_vec.T@Sw@eig_vec))
+        obj[i,0] = np.trace(alpha*(eig_vec.T@(Sb-alpha*Sw)@eig_vec))
+        obj[i,1] = alpha
+        
+    return obj
---
+++ adaptive weight MMC/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 21:21:33 2019
+
+@author: chenzu
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from func import mmc
+from func import weightmmc
+
+mu1 = np.array([0,0,0,0])
+mu2 = np.array([1,1,1,1])
+cov = np.diag([1,1,1,1])
+N = 300
+
+X1 = np.random.multivariate_normal(mu1, cov, int(N/2))
+X2 = np.random.multivariate_normal(mu2, cov, int(N/2))
+X = np.r_[X1, X2]
+gnd = np.r_[np.ones([int(N/2),1]), -np.ones([int(N/2),1])]
+
+a,b = mmc(X, gnd)
+obj = weightmmc(X,gnd,2,40)
+obj2 = weightmmc(X,gnd,3,40)
